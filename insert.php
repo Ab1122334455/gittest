8245b3cc9a382fb896f379975de6fc7c663fbe59
Ab1122334455 <35256867+Ab1122334455@users.noreply.github.com>
2018-01-12 00:03:09 +0530

    Delete insert.php

--- insert.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = pg_connect("host=localhost port=5432 dbname=ibb user=postgres password=IBB@12345");
-$query = "select * from book";
-$res = pg_query($query);
-$arr=pg_fetch_array($res);
-echo $arr["DOP"];
-?>
